b8b5381116e50003098e2ad417849d6732d37bc6
pratik4599 <p45s99tik@gmail.com>
2025-02-02 15:05:30 +0530

    update app to run on host '0.0.0.0' and port 8000

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=5000)
-
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+    )
 
     
